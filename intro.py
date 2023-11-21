6ba34cae14a768e250abd0eb04240c6f40562ba6
ASEELhayajneh88 <142718235+ASEELhayajneh88@users.noreply.github.com>
2023-11-21 23:10:03 +0300

    ...

--- intro.py
+++ intro.py
@@ -161,52 +161,519 @@
 # print(name)
 # # person 
 
-allperson= []
-person= {}
-print('welcom to our app')
-while True:
-    print('1- add new person')
-    print('2-see all person')
-    print('3-Exit')
-    choice = input('enter your choice:')
-    if choice =='1':
+# allperson= []
+# person= {}
+# print('welcom to our app')
+# while True:
+#     print('1- add new person')
+#     print('2-see all person')
+#     print('3-Exit')
+#     choice = input('enter your choice:')
+    
+#     if choice =='1':
         
-     name = input('enter your name :')
-     age= ('enter age :')
-     age= input("Enter your age: ")
-     if age.isdigit():
-      print(age)
-     else:
-         if age.isalpha():
-           print("error. Please enter a number.")
+#      name = input('enter your name :')
+#      age= ('enter age :') 
+#      age= input("Enter your age: ")
+#     #  if age.isdigit():
+#     #   print(age)
+#     #  else:
+#     #      if age.isalpha():
+#     #        print("error. Please enter a number.")
 
-     
-     job= input('enter job :')
-     
-     person['name']= name
-     person['age']= age
-     person['job']= job
-     allperson.append(person)
-     
-     print ('added sucsefully')
-    elif choice == '2':
-        counter = 1
-        for i in allperson:
-            print ('person',counter)
-            for key in i:
-             print('    ',key,':',i[key])  
-             print(i)
-        counter+=1
-    elif choice=='3':
+#      job= input('enter job :')
+#     while True: 
+#      try:
+#            age= int(input('enter age'))
+#            break
+#      except:
+#         print('please enter number')  
+    
+#     person={
+#         'name':name,
+#         'age':age,
+#         'job':job
         
-        break
+#     }
+#     allperson.append(person)
+     
+#     print ('added sucsefully')
+    
+#     elif choice =='2':                
+#     counter = 1
+#     for i in allperson:
+#             print ('person',counter)
+#             for key in i:
+#              print('    ',key,':',i[key])  
+#              print(i)
+#             counter+=1
+        
+#     break
             
      
+# list1=['a','d','b','d','e','1','2']
+# list1.sort()
+# print(list1)
+# list1.reverse()
+# print(list1)
+# print(list1.count('2'))
+# print(list1.__len__())
+# list1.pop(1)
+
+
+#method in dic: keys{}, values{},items{},get{}
+# dic1={
+#     "name":"ali",
+#     "family":"hay",
+#     "age":30,
+# }
+#key method: to get all keys in dic
+# print ('key method',dic1.keys())
+#value method: to get all keys in dic
+# print ('value',dic1.values())
+#item method: to get all keys in dic
+# print('item:',dic1.items())
+# get method:to get value of the key
+# print('get:',dic1.get('name'))
+# dic1()
+# dic1={'name':'ahmad'}
+# dic1['skills'].append('html')
+
+# print('get method :',dic1)
+# name=input('enter name')
+# passo=input('enter pass')
+# print (f"hello {name} your password is {passo}")
+
+
+# print('welcome to app')
+# i=0
+# while True:
+#     print( '__'*20)
+#     print('1-create account')
+#     print('2-login')
+#     print('3-Exit')
+#     print('4-show all person')
+#     choice = input('enter your choice:')
+    
+#     if choice =='1':
+#      name = input('enter your name :')
+#     while True:
+#         try:
+#          age=int(input('enter age:'))
+#          break
+#         except Exception as e:
+#             print(e)
+#             continue
+#     job=int(input('enter the person job:'))
+#     person={
+#         'name':name,
+#       'age':age,
+#       'job':job,
+#     }
+    
+#     allperson.append(person)
+#     print(allperson)
+#     print('you added person successfully')
+    
+#     elif choice == '2':
+#     counter=1
+#     print('__'*20)
+    
+#     print('allperson')
+#     for i in allperson:
+#      print('person',counter)
      
-  
+# allperson= []
+# person= {}           
+# print('welcome to app')
+# x=0
+# while True:
+#     print( '__'*20)
+#     print('1-create account')
+#     print('2-login')
+#     print('3-Exit')
+#     print('4-show all person')
+#     print('5-Edit a person')
+#     choice = input('enter your choice:')
+    
+#     if choice =='1':
+#         name = input('enter your name :')
+#         if allperson!=[]:
+#             isExited= True
+              
+#             for i in allperson:
+#                 if name ==i['name']:
+#                    print('this user already exist  ')
+#                    name=input('enter the user name')
+#                    isExited=True                  
+
+#                    break
+#             else :
+#                 isExited=False                   
+                    
+                    
+            
+#         while True:
+#             try:
+#                 age=int(input('enter age:'))
+#                 break
+#             except Exception as e:
+#                 print('enter a valid age')
+#                 continue
+#         job=input('enter the user job:')
+#         skills=[]
+#         while True:
+#              try:
+#                 counter2=int(input('enter the number of skills:'))
+#                 break
+#              except Exception as e:
+#                  print('enter a valid number')
+#                  continue
+#         for i in range (counter2):
+#             skill=input('enter the skill number:',i+1,':')
+#             skills.append(input())
+            
+#         while True:
+                
+#            password=input('enter the password')
+#            numberChar=False
+#            upperChar=False
+#            lowerChar=False
+#            if len(password)>=8:     
+#             for i in password:
+#                 if i.isdigit():
+#                     numberChar=True
+#                 elif i.isupper():
+#                     upperChar=True
+#                 elif i.islower():
+#                     lowerChar=True
+                
+                    
+#                     if upperChar and numberChar and lowerChar:
+#                       print("Password is strong")
+#                     break
+#                 else:
+                    
+#                     print('the password is not strong')
+#                     continue
+                
+#             else:
+#                 list1=[upperChar,lowerChar,numberChar]
+#                 list2=['upperChar','lowerChar','numberChar']
+#                 list3=[]
+#                 for i in range(len(list1)):
+#                     if list1[i]:
+#                         list3.append(list[i])
+#                         i=1
+#                 print('the password must be at least 8 characters long')
+#                 break
+
+#            person={
+#             'name':name,
+#             'age':age,
+#             'job':job,
+#             'password':password
+#           }
+#            allperson.append(person)
+#            print(allperson)
+#            print('you added person successfully')
+
+#     elif choice == '2':
+        
+        
+       
+#          print('__'*20)
+#          print('please enter your name and password')
+#          name= input('enter the user name')
+#          password=input('enter the password')
+#          for i in allperson:
+#              if i['name']==name and i['password']==password:
+#                  print('welcome',name)
+#                  break
+             
+#              else:
+#                  print('wrong username or password')
+#                  break
+             
+             
+             
+#     elif choice =='3':
+#         print('thank you for using ')
+#         break
+#     elif choice=='4':
+#         isAdmin=input('please enter the admin password')
+#         if isAdmin=='admin':
+            
+#            counter=1
+#            print('__'*20)
+    
+#            print('allperson:')
+#            for i in allperson:
+#             print('person',counter)
+#             for key in 1:   
+#                 print('   ',key,':',i[key])
+#                 counter+=1
+#             else:
+#                 print('wrong password re enter the choice')
+                 
+#            else :
+#                print('invalid option please try again')
+                      
+#         else:
+#              print('wrong choice')          
+#              x+=1
+    
+          
+# def checkLetter(letter, word, guess_word):
+ 
+#     for c in word:
+#         if c == letter:
+#             guess_word[word.index(c)] = c
+#             word[word.index(c)] = '*'
+ 
+#     return guess_word
+ 
+ 
+# word = list('hello')
+# guess_word = ['_' for x in word]
+ 
+# print(guess_word)
+# while '_' in guess_word:
+#     guess = input('Letter: ')
+#     print(checkLetter(guess, word, guess_word))
+         
+     
 
 
 
 
 
     
+     
+# print('welcome to hand man game')
+# print('start guessing')
+# word=('Aseel')
+# guesses=''
+# possible=8
+# while possible>0 :
+#     faild= 0
+#     for char in word:
+#         if char in guesses:
+#             print(char,end="")
+#         else:
+#             print("_ ",end="")
+#             faild+=1
+#             if faild==0:
+#                 print('win')
+#                 break
+#     guess = input("guess a character:")  
+#     guesses += guess 
+#     if guess not in word:
+#         possible-=1
+#         print('wrong')
+#         if possible == 0:   
+#             print('you lose')
+# import random
+
+
+
+                
+# import random
+# print ("welcome to play hangman!")
+# ## A List Of Secret Words
+# words = ['aseel','programming','full','hayajneh']
+# word = random.choice(words)
+# guesses = ''
+# possible= 5
+# while possible > 0:         
+#     failed = 0             
+#     for char in word:      
+#         if char in guesses:    
+#             print (char,end="")    
+#         else:
+#             print ("_",end=""),     
+#             failed += 1    
+#     if failed == 0:        
+#         print ("\nYou won") 
+#         break              
+# guess = input("\nguess a character:") 
+# guesses += guess                    
+# if guess not in word:  
+#         possible -= 1        
+#         print("\nWrong")    
+#         print("\nYou have", + possible, 'more guesses') 
+#         if possible == 0:           
+#             print ("\nYou Lose") 
+        
+# class person():
+#     """ هون شرح لتظهر """
+#     #multry attr>every attr is a var
+#     def __init__(self,name,age,job,skills,password):
+#         #ATTRibute name = self.varname
+#         self.name=name
+#         self.age=age
+#         self.job=job
+#         self.skills=skills
+#         self.password=password
+            
+#     def nameAndage(self):
+#         return f'name:{self.name}, age:{self.age}'
+#     def callPerson(self,person):
+#         """this method will used to call a person"""
+#         return f'{self.name} is calling {person}'
+
+
+#     def __str__(self):
+#         return f'name:{self.name},age:{self.age},job:{self.job},skills:{self.skills}'
+    
+# def checkpass():
+                
+# person1=person('Aseel','23','student','python')
+# person2=person('ali','23','eng','jave')
+# print(person1)
+# print(person2.nameAndage())
+# print(person2.callPerson('amjad')
+#Create menu item to restaurnet  whith id uniqe using A-Z ,a-z,0-9 and stored in dic.
+
+
+        
+                
+                               
+    
+        
+        
+
+
+          
+
+
+
+
+
+
+
+
+
+
+# class Restaurant:
+#  def __init__(self): 
+#        self.menudic={}
+#        self.orderlist=[]
+#        self.orderdic={}
+
+
+#  def Additem(self):
+#       id=input('Enter the id ')
+#       if  id in self.menudic:
+#        print( " the Id found")
+#       else:
+#         name=input('Enter the name:')
+#         price=int(input('Enter the price'))
+#         description=(input('enter the descripition:'))
+#         self.menudic[id]={'name':name,'price':price,'description:':description}  
+#         print('item has been added successfully')   
+#  def print_dict(self):
+#         print (self.menudic)
+        
+#  def search_item(self):
+#         id=input('Enter the id to search :')
+#         if id  in self.menudic:
+#          print ("Name of the Item",self.menudic[id]['name'])
+#          print ("Price of the Item",self.menudic[id]['price'])
+#          print ("Description of the Item",self.menudic[id]['description:'])
+#         else:
+#          print ('Item is not available')
+
+#  def delete_item(self):
+#         if id in self.menudic:
+#                 del self.menudic[id]
+#                 print ('Item deleted successfully')
+#         else:
+#                 print ('Item is not available')
+#  def placeorder(self):
+#        item= input('Please enter IDS seperated by comma ').split(',')
+#        name= input('Please enter your name: ')
+#        phone= input('Please enter your phone: ')
+#        address= input('Please enter your address: ')
+    
+#        for i in item :
+#         if i not in self.menudic:
+#          return f"item is not founf"
+     
+#        self.orderdic={'name':name,'phone':phone,'address:':address,'item':item}
+#        self.orderlist.append(self.orderdic)
+#        print("Order placed Successfully")
+#        print(self.orderlist)
+       
+
+# def vieworders(self):
+#     for order in self.orderlist:
+#      print(order) 
+                      
+        
+        
+        
+                     
+         
+# meal=Restaurant()
+# while True:        
+#  print('welcome to my little restaurent') 
+#  print('__'*20)     
+#  print('1-Add a menu item: ')
+#  print('2-Search a menu item:  ')
+#  print('3-Delete a menu item: ')
+#  print('4-List all item: ')
+#  print('5- Place an Order: ')
+#  print('6- View Orders:')
+#  print('7- Exit: ')
+#  choice = input('enter your choice: ')
+#  if choice=='1':
+#         meal.Additem()
+         
+#  elif choice == '2':
+#         meal.search_item()
+            
+#  elif choice == '3':
+#         meal.delete_item()
+        
+#  elif choice == '4':
+#         meal.print_dict()
+#  elif choice == '5':
+#         meal.placeorder()
+#  elif choice == '6':
+#         meal.vieworders()
+        
+#  elif choice == '7':
+#         break
+#  else:
+#    print("Wrong Choice ")
+         
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ restaurant mid .py
@@ -0,0 +1,94 @@
+class Restaurant:
+ def __init__(self): 
+       self.menudic={}
+       self.orderlist=[]
+       self.orderdic={}
+
+
+ def Additem(self):
+      id=input('Enter the id ')
+      if  id in self.menudic:
+       print( " the Id found")
+      else:
+        name=input('Enter the name: ')
+        price=int(input('Enter the price: '))
+        description=(input('enter the descripition: '))
+        self.menudic[id]={'name':name,'price':price,'description:':description}  
+        print('item has been added successfully')   
+ def print_dict(self):
+        print (self.menudic)
+        
+ def search_item(self):
+        id=input('Enter the id to search :')
+        if id  in self.menudic:
+         print ("Name of the Item",self.menudic[id]['name'])
+         print ("Price of the Item",self.menudic[id]['price'])
+         print ("Description of the Item",self.menudic[id]['description:'])
+        else:
+         print ('Item is not available')
+
+ def delete_item(self):
+        if id in self.menudic:
+                del self.menudic[id]
+                print ('Item deleted successfully')
+        else:
+                print ('Item is not available')
+ def placeorder(self):
+       item= input('Please enter ID ')
+       name= input('Please enter your name: ')
+       phone= input('Please enter your phone: ')
+       address= input('Please enter your address: ')
+    
+       for i in item :
+        if i not in self.menudic:
+         return f"item is not found"
+     
+       self.orderdic={'name':name,'phone':phone,'address:':address,'item':item}
+       self.orderlist.append(self.orderdic)
+       print("Order placed Successfully")
+       print(self.orderlist)
+       
+
+def vieworders(self):
+    for order in self.orderlist:
+     print(order) 
+                      
+        
+        
+        
+                     
+         
+meal=Restaurant()
+while True:        
+ print('welcome to my little restaurent') 
+ print('__'*20)     
+ print('1-Add a menu item: ')
+ print('2-Search a menu item:  ')
+ print('3-Delete a menu item: ')
+ print('4-List all item: ')
+ print('5- Place an Order: ')
+ print('6- View Orders:')
+ print('7- Exit: ')
+ choice = input('enter your choice: ')
+ if choice=='1':
+        meal.Additem()
+         
+ elif choice == '2':
+        meal.search_item()
+            
+ elif choice == '3':
+        meal.delete_item()
+        
+ elif choice == '4':
+        meal.print_dict()
+ elif choice == '5':
+        meal.placeorder()
+ elif choice == '6':
+        meal.vieworders()
+        
+ elif choice == '7':
+        break
+ else:
+   print("Wrong Choice ")
+         
+         
